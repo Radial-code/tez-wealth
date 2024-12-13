78557e0ec0aab6540c7af0d7a84414ad518a85fc
Deepak Jaat <deepakjhajhria4657@gmail.com>
2024-12-13 15:59:15 +0530

    added common heading

--- src/components/custom-ui/Heading.jsx
+++ src/components/custom-ui/Heading.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const Heading = () => {
-  return (
-    <div>Heading</div>
-  )
+const Heading = ({ children, className }) => {
+    return (
+        <h2
+            class={`font-poppins font-normal lg:text-5xl text-4xl !leading-140 text-dark-grey max-w-[501px] lg:pb-[10px] pb-2 max-custom-sm:max-w-[276px] max-md:text-center ${className}`}>
+            {children}
+        </h2>
+    )
 }
 
 export default Heading
