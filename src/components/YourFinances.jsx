d5f03788077581eb35dd280024147a6c1f57b075
Deepak Jaat <deepakjhajhria4657@gmail.com>
2024-12-14 12:16:55 +0530

    merge with dev

--- src/components/YourFinances.jsx
+++ src/components/YourFinances.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
 import Form from './common/Form'
 import Heading from './custom-ui/Heading'
+import Image from 'next/image'
+import Paragraph from './custom-ui/Paragraph'
 
 const YourFinances = () => {
     return (
@@ -8,33 +9,19 @@ const YourFinances = () => {
             <div
                 className="size-[150px] rounded-full bg-deep-sea-green blur-[70px] absolute md:block hidden left-0 opacity-20 pointer-event-none top-[-21%]">
             </div>
-            <div className="absolute lg:right-[66px] right-[-20px] lg:bottom-[41px] bottom-3"><img width="116" height="79"
-                className="lg:max-w-[116px] lg:h-[79px] max-w-[88px] h-[60px] animate-pulse"
-                src="./assets/images/svg/form-ellipse.svg" alt="form-ellipse" /></div>
+            <div className="absolute lg:right-[66px] -right-5 lg:bottom-10 bottom-3">
+                <Image width="116" height="79"
+                    className="lg:max-w-[116px] lg:h-[79px] max-w-[88px] h-[60px] animate-pulse"
+                    src="./assets/images/svg/form-ellipse.svg" alt="form-ellipse" /></div>
             <div className="container px-5 xl:px-0 max-w-[1140px]">
                 <div className="flex flex-wrap items-center justify-between">
                     <div className="lg:w-[43%] md:w-1/2 w-full xl:px-3 sm:pr-3 px-0 max-md:flex flex-col justify-center items-center">
-                        <h2
-                            className="font-poppins font-normal lg:text-5xl text-4xl !leading-140 text-dark-grey max-w-[501px] lg:pb-[10px] pb-2 max-custom-sm:max-w-[276px] max-md:text-center">
-                            <span className="text-deep-sea-green font-bold"></span>
-                        </h2>
-                        <Heading boldText="Your
+                        <Heading className="max-w-[501px] lg:pb-[10px] pb-2 max-custom-sm:max-w-[276px] max-md:text-center" boldText="Your
                                 Finances">We’re Here to Help You Take Control of </Heading>
-                        <p className="font-poppins font-normal text-base leading-160 text-light-black opacity-80 max-md:text-center">
-                            Have
-                            questions? Reach out to our team for support and personalized assistance.</p>
+                        <Paragraph className="!leading-160 !opacity-80 max-md:!text-center">Have questions? Reach out to our team for support and personalized assistance.</Paragraph>
                     </div>
                     <div className="md:w-1/2 w-full xl:px-0 px-0 lg:mt-0 mt-10">
                         <Form />
-                        <div id="popUp"
-                            className="fixed bottom-[20px] right-[20px] bg-light-green text-white p-5 rounded-lg text-center text-lg z-50 hidden">
-                            <div className="flex max-w-[300px] h-[150px]">
-                                <p className="text-white text-xl my-auto py-auto leading-normal font-poppins text-center capitalize">
-                                    message send successfully
-                                </p>
-                            </div>
-                        </div>
-
                     </div>
                 </div>
             </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,8 @@ export const ABOUT_LINK_PATH = "#about";
 export const SERVICE_LINK_PATH = "#service";
 export const CHOOSE_LINK_PATH = "#choose";
 export const FAQ_LINK_PATH = "#faq";
+export const CONTACT_LINK_PATH = "#contact";
+
+export const LINKEDIN_PATH_URL = "https://www.linkedin.com/login"
+export const TWITTER_PATH_URL = "https://x.com/?lang=en"
+export const INSTAGRAM_PATH_URL = "https://www.instagram.com/accounts/login/?next=%2Fmissionmindedmoving%2F"
