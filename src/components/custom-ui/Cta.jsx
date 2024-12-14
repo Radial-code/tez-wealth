4a090d25d3324e66796b7c6ca19180b5ba79bb37
Priyanka-390 <psr82220@gmail.com>
2024-12-14 12:03:57 +0530

    chnge in cta

--- src/components/custom-ui/Cta.jsx
+++ src/components/custom-ui/Cta.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Cta = ({ className, children, ...props }) => {
     return (
         <button {...props} href="#contact"
-            className={`relative flex items-center max-lg:hidden justify-center overflow-hidden py-3.5 text-center px-4 font-poppins rounded-full bg-light-green font-semibold text-base text-white !leading-160 transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-deep-sea-green before:duration-500 before:ease-out hover:shadow-bg-deep-sea-green hover:before:h-56 hover:before:w-full hover:border-white border-transparent border ease-linear duration-500 ${className}`}>
+            className={`relative flex items-center justify-center overflow-hidden py-3.5 text-center px-4 font-poppins rounded-full bg-light-green font-semibold text-base text-white !leading-160 transition-all before:absolute before:h-0 before:w-0 before:rounded-full before:bg-white before:duration-500 before:ease-out hover:shadow-bg-deep-sea-green hover:before:h-56 hover:before:w-full hover:border-light-green hover:text-light-green border-transparent border-2 ease-linear duration-500 ${className}`}>
             <span className="relative z-10">{children}</span>
         </button>
     )
